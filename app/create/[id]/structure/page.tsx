cea2f5dd760c0dec278c2f92036e3c35f071cfde
Raphaelo Ocitti <raphaelocitti@gmail.com>
2025-01-30 22:43:33 +0300

    finished project

--- app/create/[id]/structure/page.tsx
+++ app/create/[id]/structure/page.tsx
@@ -5,22 +5,19 @@ import { SelectCategory } from "@/app/components/SelectedCategory";
 
 
 
-export default function StructureRoute({ params }: {params: {id: string} }){
+export default function StructureRoute({ params }: { params: { id: string } }) {
     return (
         <>
-        <div className="w-3/5 mx-auto">
-            <h2 className="text-3xl font-semibold tracking-tight transition-colors ">Which of the best describes your Hostel?</h2>
+          <div className="w-3/5 mx-auto">
+            <h2 className="text-3xl font-semibold tracking-tight transition-colors ">
+              Which of the best describes your Hostel?
+            </h2>
         </div>
 
-        <form action={createCategoryPage}>
-
+          <form action={createCategoryPage}>
             <input type="hidden" name="hostelId" value={params.id} />
             <SelectCategory />
-            
             <CreateBottomBar />
-            
-
-            
         </form>
         </>
     );
