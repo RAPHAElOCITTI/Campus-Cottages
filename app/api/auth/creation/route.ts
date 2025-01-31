8a2d83a20a928eb87998fd0f428ff36913700842
Raphaelo Ocitti <raphaelocitti@gmail.com>
2025-01-31 20:10:49 +0300

    finished project

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -32,5 +32,5 @@ export async function GET(){
         });
     }
 
-    return NextResponse.redirect(`http://localhost:3000`);
+    return NextResponse.redirect(`https://campus-cottages.vercel.app`);
 }
