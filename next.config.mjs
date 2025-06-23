e25cb3052517159f6deec95226477ab0100db1c6
RAPHAELOCITTI <raphaelocitti@gmail.com>
2025-06-23 23:36:29 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
- 
-const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['my-proxy.com', '*.my-proxy.com'],
-      bodySizeLimit: '3mb',
-      // Note: Setting serverActions to 'true' explicitly when other options are present
-      // means that server actions are enabled and these options apply to them.
-      // If 'true' was the *only* setting, it would just enable them with defaults.
-      // When combining, you essentially enable them and then provide the specific configurations.
-      // You don't need to explicitly add `true` if you're already providing options like allowedOrigins or bodySizeLimit,
-      // as the presence of these options implies server actions are being configured and thus enabled.
-    },
-  },
-};
 
 module.exports = nextConfig;
 const nextConfig= {
@@ -46,6 +31,7 @@ const nextConfig= {
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', 'campuscottages.vercel.app']
+      bodySizeLimit: '5mb',
     }
   },
 
